bce2b3acee8356449368edcb28598994d2890610
xin.luo <xin.luo@happyelements.com>
2017-05-12 00:03:57 +0800

    update

---
+++ Game/Assets/Scripts/NodeController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeController : MonoBehaviour {
+    float circleRadius;
+	// Use this for initialization
+	void Start () {
+        circleRadius = GetComponent<CircleCollider2D>().radius * transform.localScale.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        DrawTool.DrawCircle(transform, transform.localPosition, circleRadius); 
+	}
+        
+}
--- Game/Assets/Scripts/PlayerController.cs
+++ Game/Assets/Scripts/PlayerController.cs
@@ -4,29 +4,53 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 	public GameObject targetNode;		//target node to get swing
+	bool connectState = true;
+    float speed = 10;
 	// Use this for initialization
 	void Start () {
-		
+        resetVelocity(speed);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		ClickEvent ();
+        DrawTool.DrawLine(transform, transform.position, GetComponent<DistanceJoint2D>().connectedBody.transform.position);
 	}
+
+    void resetVelocity(float speed) { 
+        Vector2 vec = gameObject.GetComponent<Rigidbody2D>().velocity.normalized;
+        gameObject.GetComponent<Rigidbody2D>().velocity = vec * speed;
+    }
 
 	void ClickEvent(){
 		if (Input.GetMouseButtonDown(0)) {
-			if (GetComponent<DistanceJoint2D> ()) {
-				Destroy (GetComponent<DistanceJoint2D> ());
-			}
-			DistanceJoint2D joint = gameObject.AddComponent<DistanceJoint2D> ();
-			joint.connectedBody = targetNode.GetComponent<Rigidbody2D> ();
-			Debug.Log ("aaaa");
+            //if (connectState)
+            //{
+            //    connectState = !connectState;
+            //    releaseRope();
+            //}
+            //else
+            //{
+            //    connectState = !connectState;
+            //    makeRope();
+            //}
+            releaseRope();
+            makeRope();
 		}
 	}
 
+	void releaseRope(){
+		if (GetComponent<DistanceJoint2D> ()) {
+			Destroy (GetComponent<DistanceJoint2D> ());
+		}
+	}
+
+	void makeRope(){
+		DistanceJoint2D joint = gameObject.AddComponent<DistanceJoint2D> ();
+		joint.connectedBody = targetNode.GetComponent<Rigidbody2D> ();
+        //resetVelocity(speed);
+	}
 	void OnTriggerEnter2D(Collider2D col){
-		Debug.Log ("bbbb");
 		targetNode = col.gameObject;
 	}
 }
