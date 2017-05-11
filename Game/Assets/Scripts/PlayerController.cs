782c9bac617c22540830181dba5266354057caca
xin.luo <xin.luo@happyelements.com>
2017-05-11 20:06:53 +0800

    up

---
+++ Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	public GameObject targetNode;		//target node to get swing
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ClickEvent ();
+	}
+
+	void ClickEvent(){
+		if (Input.GetMouseButtonDown(0)) {
+			if (GetComponent<DistanceJoint2D> ()) {
+				Destroy (GetComponent<DistanceJoint2D> ());
+			}
+			DistanceJoint2D joint = gameObject.AddComponent<DistanceJoint2D> ();
+			joint.connectedBody = targetNode.GetComponent<Rigidbody2D> ();
+			Debug.Log ("aaaa");
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+		Debug.Log ("bbbb");
+		targetNode = col.gameObject;
+	}
+}
