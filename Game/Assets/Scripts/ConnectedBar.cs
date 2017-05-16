7f747243f034ca7fa9e71a2c18aa1e2f501820ce
xin.luo <xin.luo@happyelements.com>
2017-05-17 02:36:13 +0800

    new method for control

---
+++ Game/Assets/Scripts/ConnectedBar.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConnectedBar : MonoBehaviour {
+    public GameObject button1;
+    public GameObject button2;
+    float beginLength;
+	// Use this for initialization
+	void Start () {
+        beginLength = (button1.transform.position - button2.transform.position).magnitude;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 pos = (button1.transform.position + button2.transform.position) / 2;
+        transform.position = new Vector3(pos.x, pos.y, 0);
+        float length = (button1.transform.position - button2.transform.position).magnitude;
+        Vector3 scale = transform.localScale;
+        transform.localScale = new Vector3(scale.x,length/beginLength, scale.z);
+        float angle = Vector2.Angle(button1.transform.position - button2.transform.position, Vector2.right);
+        if (button1.transform.position.y < button2.transform.position.y)
+            angle *= -1;
+        transform.rotation = Quaternion.AngleAxis(90+angle, Vector3.forward);
+	}
+
+
+}
---
+++ Game/Assets/Scripts/OpenControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenControl : MonoBehaviour {
+    float maxDist;
+    public GameObject tail;
+	// Use this for initialization
+	void Start () {
+        maxDist = 10f;
+        GetComponent<DistanceJoint2D>().autoConfigureDistance = false;
+        tail.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        open();
+	}
+
+    void open() {
+        float distance = GetComponent<DistanceJoint2D>().distance;
+        if (distance + 0.2 <= maxDist) {
+            GetComponent<DistanceJoint2D>().distance += 0.5f;
+        }
+    }
+}
